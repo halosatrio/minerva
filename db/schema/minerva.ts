508a81afbb0f686e2a81f9841bbb46c6f951388e
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-05-09 12:36:15 +0700

    feat: define schema

--- db/schema/minerva.ts
+++ db/schema/minerva.ts
@@ -3,32 +3,62 @@ import {
   index,
   timestamp,
   date,
-  uuid,
   boolean,
   integer,
   pgSchema,
+  smallint,
+  serial,
+  varchar,
+  primaryKey,
 } from "drizzle-orm/pg-core";
 
-export const mySchema = pgSchema("bebop");
-
-export const habits = mySchema.table("habits", {
-  id: uuid("id").primaryKey(),
-  userId: uuid("user_id").notNull(),
-  title: text("title").notNull(),
-  icon: text("icon").notNull(),
-  color: text("color").notNull(),
-  isActive: boolean("is_active").notNull(),
-  startDate: date("start_date").notNull(),
-  dailyGoal: integer("daily_goal").notNull(),
-  weeklyGoal: integer("weekly_goal").notNull(),
-  createdAt: timestamp("created_at").defaultNow(),
-  updatedAt: timestamp("updated_at").defaultNow(),
-});
+export const mySchema = pgSchema("minerva");
 
 export const users = mySchema.table("users", {
-  id: uuid("id").primaryKey(),
-  email: text("email").notNull(),
+  id: serial("id").primaryKey(),
+  email: text("email").unique().notNull(),
   password: text("password").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export const habits = mySchema.table(
+  "habits",
+  {
+    id: serial("id").primaryKey(),
+    userId: integer("user_id").references(() => users.id, {
+      onDelete: "cascade",
+    }),
+    title: varchar("title", { length: 256 }).notNull(),
+    icon: varchar("icon", { length: 256 }).notNull(),
+    color: varchar("color", { length: 10 }).notNull(),
+    isActive: boolean("is_active").default(true),
+    startDate: date("start_date").notNull(),
+    dailyGoal: integer("daily_goal").notNull(),
+    weeklyGoal: integer("weekly_goal").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (habits) => {
+    return {
+      userIdIndex: index("name_idx").on(habits.userId),
+    };
+  }
+);
+
+export const tracker = mySchema.table(
+  "tracker",
+  {
+    date: date("date").notNull(),
+    habitId: integer("habit_id").references(() => habits.id, {
+      onDelete: "cascade",
+    }),
+    count: smallint("count").notNull(),
+    updatedAt: timestamp("updated_at"),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.date, table.habitId] }),
+    };
+  }
+);
