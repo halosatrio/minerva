77326593405032e35d651961b12b61cce9d285f9
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-05-10 00:46:57 +0700

    feat: implement jwt on auth endpoint

--- routes/auth.ts
+++ routes/auth.ts
@@ -1,4 +1,4 @@
-import bcrypt from "bcrypt";
+import { sign } from "hono/jwt";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { db } from "../db";
@@ -23,16 +23,14 @@ export const authRoutes = new Hono()
     async (c) => {
       const body = c.req.valid("json");
 
-      const validatedUser = actionUserSchema.parse(body);
-
-      const bcryptHash = await Bun.password.hash(validatedUser.password, {
+      const bcryptHash = await Bun.password.hash(body.password, {
         algorithm: "bcrypt",
         cost: 10,
       });
 
       let res = await db
         .insert(usersTable)
-        .values({ email: validatedUser.email, password: bcryptHash })
+        .values({ email: body.email, password: bcryptHash })
         .returning()
         .then((res) => res[0]);
 
@@ -59,8 +57,6 @@ export const authRoutes = new Hono()
     async (c) => {
       const body = c.req.valid("json");
 
-      const validatedUser = actionUserSchema.parse(body);
-
       const user = await db
         .select({
           id: usersTable.id,
@@ -68,16 +64,25 @@ export const authRoutes = new Hono()
           password: usersTable.password,
         })
         .from(usersTable)
-        .where(eq(usersTable.email, validatedUser.email));
+        .where(eq(usersTable.email, body.email));
 
       if (user.length !== 0) {
         const isMatch = await Bun.password.verify(
-          validatedUser.password,
+          body.password,
           user[0].password
         );
 
+        // GENERATE JWT (token)
+        const secret = process.env.SECRET_KEY;
+        const tokenPayload = {
+          sub: body.id,
+          email: body.email,
+          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24, // Token expires in 24 hours
+        };
+        const token = await sign(tokenPayload, secret!);
+
         if (isMatch) {
-          return c.json({ status: 200, data: user });
+          return c.json({ status: 200, message: "Success!", data: token });
         } else {
           return c.json({ status: 500, data: "password is incorrect!" });
         }
