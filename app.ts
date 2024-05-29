c8de0f34195fb6bae9901f0f516bc97cf1c5473f
halosatrio <63773815+halosatrio@users.noreply.github.com>
2024-05-29 09:22:29 +0700

    feat: add cors localhost for dev

--- app.ts
+++ app.ts
@@ -3,10 +3,17 @@ import { logger } from "hono/logger";
 import { habitRoutes } from "./routes/habit";
 import { authRoutes } from "./routes/auth";
 import { tracker as trackerRoute } from "./routes/tracker";
+import { cors } from "hono/cors";
 
 const app = new Hono();
 
 app.use("*", logger());
+app.use(
+  "*",
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 const apiRoutes = app
   .basePath("/api")
